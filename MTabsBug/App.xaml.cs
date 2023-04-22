6305c6916ac7e2563f93ef656381de956d497297
Yu-Core <3105136125@qq.com>
2023-04-23 05:55:06 +0800

    添加项目文件。

---
+++ MTabsBug/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MTabsBug;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ MTabsBug/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MTabsBug;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
